4d1c9cc94b2ae9236b8d75abf6334b8ff0781697
britneyliamaulana <maulanathandeka@gmail.com>
2026-02-02 17:34:26 +0200

    commit 	modified:   chamber/join.html 	renamed:    scripts/join.js -> chamber/scripts/join.js 	renamed:    scripts/thankyou.js -> chamber/scripts/thankyou.js 	modified:   chamber/styles/large.css 	modified:   chamber/styles/small.css

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -5,4 +5,10 @@ document.getElementById("lname").textContent = params.get("lname");
 document.getElementById("email").textContent = params.get("email");
 document.getElementById("phone").textContent = params.get("phone");
 document.getElementById("business").textContent = params.get("business");
-document.getElementById("timestamp").textContent = params.get("timestamp");
+
+const rawTimestamp = params.get("timestamp");
+
+if (rawTimestamp) {
+    const formatted = new Date(rawTimestamp).toLocaleString();
+    document.getElementById("timestamp").textContent = formatted;
+}
